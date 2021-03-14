5baadbef1900cb770b05dc99618bd97629945be3
Constance Huang <constancehuang@MacBook-Pro.local>
2021-03-14 16:09:42 -0700

    added movie query funtions to crud.py

--- crud.py
+++ crud.py
@@ -39,6 +39,17 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
+def get_movie_by_id(movie_id):
+    """Return a movie by primary key."""
+
+    return Movie.query.get(movie_id)
+    
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
