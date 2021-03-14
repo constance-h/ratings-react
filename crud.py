7852a987e3733902e48d745eaa85054865f037f9
Constance Huang <constancehuang@MacBook-Pro.local>
2021-03-14 16:08:24 -0700

    added user search functions to crud.py

--- crud.py
+++ crud.py
@@ -12,6 +12,23 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    """Return all users."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+
+    return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return new movie"""
 
@@ -31,6 +48,7 @@ def create_rating(user, movie, score):
     db.session.commit()
 
     return rating
+
 
 
 
